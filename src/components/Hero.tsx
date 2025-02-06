e9a50dac5138331190d16e0d0a458a8181a0e3eb
benjaminhaag <141918984+benjaminhaag@users.noreply.github.com>
2025-02-06 09:40:08 +0000

    Bugfixes (#15)
    minor fixes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,21 +10,21 @@ import NextLink from 'next/link';
 
 export default function Hero() {
   return (
-    <Parallax className='h-dvh'>
+    <Parallax className='h-svh'>
       <Layer speed={0.1}>
-        <div className='relative h-dvh w-full'>
+        <div className='relative h-svh w-full'>
         <Image alt="Background Image" className='object-cover' src={Background} fill />
         </div>
       </Layer>
       <Layer speed={0.2}>
-        <div className='relative h-dvh w-full'>
+        <div className='relative h-svh w-full'>
           <Container className='h-full pt-12 items-center justify-end hidden md:flex'>
             <Image alt="Background Image" sizes='100vw' className='h-[150%] w-auto -mb-[40%] -mr-[10%]' src={Box} />
           </Container>
         </div>
       </Layer>
       <Layer speed={0.5}>
-        <div className='relative h-dvh w-full flex items-center justify-start'>
+        <div className='relative h-svh w-full flex items-center justify-start'>
           <Container>
             <div className='md:w-[50%] text-center md:text-left'>
               <h1 className="text-4xl md:text-6xl pb-10">Expert Solutions for<br /><span className="text-primary"><Typed /></span><span className="text-secondary">.</span></h1>
